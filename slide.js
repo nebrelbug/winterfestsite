0b895b8e2ed544bad068c4cec0e3021355b0b937
JennyKiener <46326573+JennyKiener@users.noreply.github.com>
2019-01-17 09:50:47 -0700

    Slideshow works

--- slide.js
+++ slide.js
@@ -1,8 +1,10 @@
-var slideIndex = [1,1];
+var slideIndex = [1,1,1,1];
 /* Class the members of each slideshow group with different CSS classes */
 var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4"] 
 showSlides(1, 0);
 showSlides(1, 1);
+showSlides(1, 2);
+showSlides(1, 3);
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
